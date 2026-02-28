8d3b6e40278719e23d4934846cc64700303e52e3
acoder49 <152408691+Akshat6133@users.noreply.github.com>
2026-02-28 20:55:47 +0530

    Update comment for KDE panel transparency
    Clarified comment regarding KDE task manager panel transparency.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,15 @@
 # tmux
 # zsh config (including oh-my-zsh)
 
-# to make the panel transparent/translucent in kde
+# to make the task manager panel transparent/translucent in kde
 #https://github.com/luisbocanegra/plasma-panel-colorizer?tab=readme-ov-file#manually
 # paru -S plasma6-applets-panel-colorizer 
+
+#[konsole configs]
+#theme: kde-story
+#font: monospace 13pt
+#command: /usr/bin/zsh
+#initial Directory: ~/Code/qemu_stuff/src
+
+# [desktop wallpapers]
+# ~/Code/wallpapers_local (same in the macos downloads/wallpapers_local)
